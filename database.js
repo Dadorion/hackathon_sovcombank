cf5ebbd8820586065d978904c6af030fdbb0c13d
Ilia Kaziamov <kaziamov@outlook.com>
2023-05-25 11:17:30 +0300

    Update env variables

--- database.js
+++ database.js
@@ -5,10 +5,10 @@ const { Pool } = pkg
 
 const config = {
    user: process.env.DB_USER || "postgres",
-   password: process.env.DB_PASSWORD || "1111",
+   password: process.env.DB_PASS || "1111",
    host: process.env.DB_HOST || "localhost",
    port: process.env.DB_PORT || 5432,
-   database: process.env.DB_DB || "recruitment",
+   database: process.env.DB_NAME || "recruitment",
 }
 const pool = new Pool(config)
 export default pool
