c1e63cbdcf5f77ac478592913182ef0be5b26a0b
Ilia Kaziamov <kaziamov@outlook.com>
2023-05-28 22:19:07 +0300

    Fix connect error
    In database.js .env file read uncorrect

--- back/database.js
+++ back/database.js
@@ -6,7 +6,8 @@ const { Pool } = pkg
 const config = {
    user: process.env.DB_USER || "postgres",
    password: process.env.DB_PASSWORD || "1111",
-   host: process.env.DB_HOST || "localhost",
+   // переменная хоста не прогружается из .env файла корректно. надо будет как то пофиксить
+   host: "db",
    port: process.env.DB_PORT || 5432,
    database: process.env.DB_DB || "recruitment",
 }
